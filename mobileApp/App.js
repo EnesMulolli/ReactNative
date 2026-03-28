45c4392dcc4ba78f04dc1a471c54762aac2310ec
EnesMulolli <enesmulolli59@gmail.com>
2026-03-28 12:48:20 +0100

    Button, touchableOpacity

--- mobileApp/App.js
+++ mobileApp/App.js
@@ -5,6 +5,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import MainScreen from './MainScreen';
 import ExcerciseScreen from './screens/ExcerciseScreen';
 import ListScreen from './screens/ListScreen';
+import ButtonScreen from './screens/ButtonScreen';
 
 
 
@@ -14,11 +15,12 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-      initialRouteName='List'
+      initialRouteName='Button'
       screenOptions={{
         headerTitle: 'APP',
       }}
       >
+        <Stack.Screen name="Button" component={ButtonScreen}/>
         <Stack.Screen name="Exercise" component={ExcerciseScreen}/>
         <Stack.Screen name="Main" component={MainScreen}/>
         <Stack.Screen name="List" component={ListScreen}/> 
