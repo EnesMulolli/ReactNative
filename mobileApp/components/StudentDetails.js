b513cdb2d52460f7d80401ffa52bcbecfd375a6f
EnesMulolli <enesmulolli59@gmail.com>
2026-05-02 11:09:11 +0200

    added alert when a student is clicked

--- mobileApp/components/StudentDetails.js
+++ mobileApp/components/StudentDetails.js
@@ -1,58 +1,53 @@
 import React from "react";
-import { Text, StyleSheet, View, Image } from 'react-native';
+import { Text, StyleSheet, View, Image, TouchableOpacity } from "react-native";
 
-const StudentDetails = (props) => {
-    return (
-        <View style={styles.container}>
-            <View style={styles.cardWrapper}>
+const StudentDetails = ({ name, image, description, onPress }) => {
+  return (
+    <TouchableOpacity
+      onPress={() => onPress(name)}
+      style={styles.container}
+      activeOpacity={0.7}
+    >
+      <View style={styles.cardWrapper}>
+        <Image source={image} style={styles.img} />
 
-                <Image source={props.image} style={styles.img} />
-
-                <View style={styles.infoWrapper}>
-                    <Text style={styles.name}>{props.name}</Text>
-                    <Text style={styles.description}>
-                        {props.description}
-                    </Text>
-                </View>
-
-            </View>
+        <View style={styles.infoWrapper}>
+          <Text style={styles.name}>{name}</Text>
+          <Text style={styles.description}>{description}</Text>
         </View>
-    )
-}
+      </View>
+    </TouchableOpacity>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        marginBottom: 15
-    },
-
-    cardWrapper: {
-        flexDirection: 'row',
-        backgroundColor: 'white',
-        borderRadius: 8,
-        alignSelf: 'center',
-        width: '90%',
-        padding: 10
-    },
-
-    img: {
-        width: 100,
-        height: 100,
-        borderRadius: 8
-    },
-
-    infoWrapper: {
-        flex: 1,
-        marginLeft: 15
-    },
-
-    name: {
-        fontWeight: 'bold',
-        marginBottom: 5
-    },
-
-    description: {
-        flexShrink: 1
-    }
+  container: {
+    marginBottom: 15,
+    alignSelf: "center",
+    width: "90%",
+  },
+  cardWrapper: {
+    flexDirection: "row",
+    backgroundColor: "white",
+    borderRadius: 8,
+    padding: 10,
+  },
+  img: {
+    width: 100,
+    height: 100,
+    borderRadius: 8,
+  },
+  infoWrapper: {
+    flex: 1,
+    marginLeft: 15,
+  },
+  name: {
+    fontWeight: "bold",
+    marginBottom: 5,
+  },
+  description: {
+    flexShrink: 1,
+  },
 });
 
 export default StudentDetails;
